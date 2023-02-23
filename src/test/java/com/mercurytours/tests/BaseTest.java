8382b09f5582f4d3cb06e355ae051b423872e9b2
SoniS-lab <79753178+SoniS-lab@users.noreply.github.com>
2023-02-23 08:57:25 +0530

    Update BaseTest.java

--- src/test/java/com/mercurytours/tests/BaseTest.java
+++ src/test/java/com/mercurytours/tests/BaseTest.java
@@ -63,7 +63,7 @@ public class BaseTest
     @AfterMethod
     public void teatDown()
     {
-        System.setProperty("BROWSER") = null;
+        System.setProperty("BROWSER","null");
         driver.quit();
     }
 }
